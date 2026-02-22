6cad506c8941a17f59b5ae57ed2e18c5a05de5df
Mahmoud_Nasser <mahmoudnasser80m@gmail.com>
2026-02-23 01:18:25 +0200

    ThSection

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import SecSection from "./components/SecSection";
+import ThSection from "./components/ThSection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <SecSection />
+      <ThSection />
     </div>
   );
 };
